2c532d9515fe12a19c441bea3fae55417abb3986
Mandeep Mehra <mandeep.mehra@xebia.com>
2020-03-05 12:28:13 +0530

    added ec2 instance creation

--- ecs.tf
+++ ecs.tf
@@ -1,6 +1,3 @@
-
-
-
 data "aws_iam_role" "iamrole" {
   name = "AWSServiceRoleForECS"
 }
@@ -11,6 +8,24 @@ data "aws_lb_target_group" "tg" {
 
 resource "aws_ecs_cluster" "ecscluster" {
   name = "deepdive"
+}
+
+# Use ecsInstanceRole for attaching to EC2 Instance
+
+# TBD: Create EC2 Instance with User Data (refer file) - Use AMI - ami-2b3b6041 
+resource "aws_instance" "ecscontainer" {
+  ami           = "ami-2b3b6041"
+  instance_type = "t2.micro"
+  key_name      = "ecsinstance" # Change as per your system, Optional
+  tags = {
+    Name = "ECSINSTANCE"
+  }
+  iam_instance_profile = "ecsInstanceRole"
+  user_data = <<-EOF
+              #!/bin/bash
+              yum install -y aws-cli
+              aws s3 cp s3://mmecsdeepdive/ecs.config /etc/ecs/ecs.config
+              EOF
 }
 
 resource "aws_ecs_task_definition" "taskdef" {
